b057da9bf9fc9482b8f232bdfeb2e2217477e563
Bruno Benicio <brunobenicio2214@gmail.com>
2015-09-18 21:36:09 -0300

    Fix edit functionality

--- java/com/beno/reclamator/RegisterCompanyActivity.java
+++ java/com/beno/reclamator/RegisterCompanyActivity.java
@@ -1,5 +1,6 @@
 package com.beno.reclamator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -23,7 +24,7 @@ public class RegisterCompanyActivity extends AppCompatActivity {
 
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			edit = extras.getParcelable("edit");
+			edit = extras.getParcelable(EDIT_EXTRA);
 		}
 
 		Button saveButton = (Button) findViewById(R.id.saveButton);
@@ -35,21 +36,45 @@ public class RegisterCompanyActivity extends AppCompatActivity {
 		});
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (edit != null) {
+			setEditText(R.id.name, edit.name);
+			setEditText(R.id.phone, edit.phoneNumber);
+			setEditText(R.id.email, edit.email);
+			setEditText(R.id.address, edit.address);
+			setEditText(R.id.website, edit.website);
+		}
+	}
+
 	private String getEditText(int id) {
 		return ((EditText) findViewById(id)).getText().toString();
+	}
+	private void setEditText(int id, String text) {
+		((EditText) findViewById(id)).setText(text);
 	}
 
 	private void save() {
 		Company company = new Company(getEditText(R.id.name), getEditText(R.id.phone), getEditText(R.id.email),
 				                      getEditText(R.id.address), getEditText(R.id.website));
 
+		if (!company.website.startsWith("http://") && !company.website.startsWith("https://")) {
+			company.website = "http://" + company.website;
+		}
+
 		DatabaseWriter writer = new DatabaseWriter(new DatabaseHelper(this));
 		if (edit != null) {
 			writer.update(company);
+			writer.close();
+			Intent intent = new Intent();
+			intent.putExtra(CompanyDescActivity.COMPANY_EXTRA, company);
+			setResult(RESULT_OK, intent);
 		} else {
 			writer.insert(company);
+			writer.close();
 		}
-		writer.close();
 		Company.reloadCompanies();
 		finish();
 	}
